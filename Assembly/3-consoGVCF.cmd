350be8097e5165f6e17fd45a6254c4d56db04fbf
Javier Sala-Garcia <33962787+mataman2017@users.noreply.github.com>
2022-12-12 17:23:36 +0100

    Update 3-consoGVCF.cmd
    Import single-sample GVCFs into GenomicsDB before joint genotyping

--- Assembly/3-consoGVCF.cmd
+++ Assembly/3-consoGVCF.cmd
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-#SBATCH -J consoGVCF
-##SBATCH --test-only
-#SBATCH -N 1
-##SBATCH -n 4
-#SBATCH -c 4
-##SBATCH --mem-per-cpu=5GB
-#SBATCH --mem 230GB
-#SBATCH -t 6-00:00:00
-#SBATCH -D /mnt/lustre/scratch/nlsas/home/csic/bbe/esl/1-Datos/4-ConsoGVCF
-#SBATCH -o /home/csic/bbe/esl/2-Envios/1-Muestras_Maria/3-OE-gvcf2conso/%x-%N-%j.out
-#SBATCH -e /home/csic/bbe/esl/2-Envios/1-Muestras_Maria/3-OE-gvcf2conso/%x-%N-%j.err
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=enrique.saez@mncn.csic.es
+OUT=/mnt/DATA/B_JUN_reseq/H_mitocondrial/D_geno/B_VCFs
+INFILE=/mnt/DATA/B_JUN_reseq/H_mitocondrial/C_bam2gvcf/B_gvcf
+GENOME=/mnt/DATA/0_REF_genome/Dryad/fasta/B_mitochondrion/mitochondrion.fasta
+READS=$(find $INFILE -name "*.g.vcf.gz")
+
+### loop to inflate the variant option
+OPTION=""
+for i in $READS
+do
+	OPTION="${OPTION} -V ${i}"
+done
+echo $OPTION
 
 
-module load cesga/2020 gatk/4.2.0.0
-
-time gatk --java-options "-DGATK_STACKTRACE_ON_USER_EXCEPTION=true -Xmx200g -Xms200g" GenomicsDBImport \
- --genomicsdb-workspace-path alljuncos240DB \
- -L /home/csic/bbe/esl/0-Soft_y_Utils/2-Utiles/chr_30zSplit.bed \
- --sample-name-map /home/csic/bbe/esl/0-Soft_y_Utils/2-Utiles/alljuncos240.sample_map \
- --tmp-dir $LUSTRE_SCRATCH \
- --genomicsdb-shared-posixfs-optimizations true \
+gatk --java-options "-DGATK_STACKTRACE_ON_USER_EXCEPTION=true" GenomicsDBImport \
+ --genomicsdb-workspace-path MITO_alljuncos240DB \
+ -L /mnt/DATA/B_JUN_reseq/H_mitocondrial/D_geno/A_cmd/chr_mitoch.bed \
+ $OPTION \
  --create-output-variant-md5 true \
  --max-num-intervals-to-import-in-parallel 10
+
