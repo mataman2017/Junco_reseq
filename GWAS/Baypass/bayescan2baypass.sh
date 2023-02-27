2b47a7e7003668c7122235b3b65f73873622a52c
Javier Sala-Garcia <33962787+mataman2017@users.noreply.github.com>
2023-02-27 16:00:37 +0100

    Update and rename lala.cmd to bayescan2baypass.sh
    script to parse a bayescan file to a baypass format

---
+++ GWAS/Baypass/bayescan2baypass.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## 17/02/22 Javier Sala
+## To run the script: sh bayescan2baypass.sh *name_of_bayescan_file*
+
+awk '/^\[pop\]=/{out=substr($0, 7) "_tmp_baypass.txt"} {if(out) print $4, $5 > out}' $1
+paste $(ls -v *_tmp_baypass.txt) | sed 's/\t/ /g' | tail -n+2 > baypass_infile.geno
+printf "\nThe order in which 'paste' pasted each file to baypass_infile is:\n"
+printf "$(ls -v *_tmp_baypass.txt) \n\n"
+rm -f *_tmp_baypass*
