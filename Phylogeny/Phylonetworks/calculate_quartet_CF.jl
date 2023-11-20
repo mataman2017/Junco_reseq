73aaed6971477176abeb6e0add353784e185815d
Javier Sala-Garcia <33962787+mataman2017@users.noreply.github.com>
2023-11-20 13:00:26 -0500

    Create calculate_quartet_CF.jl

---
+++ Phylogeny/Phylonetworks/calculate_quartet_CF.jl
@@ -0,0 +1,25 @@
+#!/bin/env julia
+
+mappingfile = "pop.txt"
+
+using DataFrames
+
+tm = CSV.read(mappingfile, DataFrame, header=true, delim='\t')
+
+taxonmap = Dict(row[:individual] => row[:species] for row in eachrow(tm))
+
+using PhyloNetworks
+
+df_sp = writeTableCF(q, t)
+
+CSV.write("tableCF_species.csv", df_sp)
+
+genetrees = readMultiTopology("Trees_concat_astral_225_2-20_FILT_miss_auto_neutral_GC.txt");
+
+q, t = countquartetsintrees(genetrees, taxonmap, showprogressbar=true)
+
+using DataFrames
+
+df_sp = writeTableCF(q, t)
+
+CSV.write("tableCF_species.csv", df_sp)
