7b952ebd8d0431cf558278d165ed7a315e873a2e
Javier Sala-Garcia <33962787+mataman2017@users.noreply.github.com>
2023-11-20 15:37:17 -0500

    Update array_SNaQ.cmd

--- Phylogeny/Phylonetworks/array_SNaQ.cmd
+++ Phylogeny/Phylonetworks/array_SNaQ.cmd
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --output=/projects/VONHOLDT/jsala/C_Phylo/E_Phylonetworks/OE/%x-N%N-J%j-%t.out
-#SBATCH --error=/projects/VONHOLDT/jsala/C_Phylo/E_Phylonetworks/OE/%x-N%N-J%j-%t.err
+#SBATCH --error=/projects/VONHOLDT/jsala/C_Phylo/E_Phylonetworks/OE/runsnaq_slurm%a.err
+#SBATCH -o /projects/VONHOLDT/jsala/C_Phylo/E_Phylonetworks/OE/runsnaq_slurm%a.log
 #SBATCH -J runsnaq
-#SBATCH --array=0-2
-#SBATCH -c 10
-#SBATCH --mem=16G
+#SBATCH --array=0-10
+#SBATCH -c 30
+#SBATCH --mem=32G
+#SBATCH --time=60:00:00
 
 ## --array: to run multiple instances of this script,
 ##          one for each value in the array.
@@ -15,5 +16,5 @@
 echo "slurm task ID = $SLURM_ARRAY_TASK_ID used as hmax"
 echo "start of SNaQ parallel runs on $(hostname)"
 # finally: launch the julia script, using Julia executable appropriate for slurm, with full paths:
-~/bin/julia --history-file=no -- runSNaQ.jl $SLURM_ARRAY_TASK_ID 30 > net$SLURM_ARRAY_TASK_ID_30runs.screenlog 2>&1
+/home/jg2334/download/julia-1.9.4/bin/julia --history-file=no -- runSNaQ.jl $SLURM_ARRAY_TASK_ID 30 > net$SLURM_ARRAY_TASK_ID_30runs.screenlog 2>&1
 echo "end of SNaQ run ..."
