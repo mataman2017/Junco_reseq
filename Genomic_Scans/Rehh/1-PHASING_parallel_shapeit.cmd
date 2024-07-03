a2998de1ba7c4b779ba19093f9c55a6f13057108
Javier Sala-Garcia <33962787+mataman2017@users.noreply.github.com>
2024-07-03 11:27:28 +0200

    Update 1-PHASING_parallel_shapeit.cmd

--- Genomic_Scans/Rehh/1-PHASING_parallel_shapeit.cmd
+++ Genomic_Scans/Rehh/1-PHASING_parallel_shapeit.cmd
@@ -1,15 +1,31 @@
-#!/bin/bash
+REC=/mnt/DATA/B_JUN_reseq/N_genetic_map/E_GOOD_rmap/D_rmap_shapeit_format/rmap_
+DIR=/mnt/DATA/B_JUN_reseq/A_chr_split/B_VCFs/E_JUN_227/chromosome
+OUT=/mnt/DATA/B_JUN_reseq/A_chr_split/B_VCFs/E_JUN_227/C_polarize_correct
+CHR=$(find $DIR -name "*.vcf.gz" )
 
-## Phasing chromosome VCF's with shapeit, parallel run. 
+SHAPEIT=/home/milab/lib/shapeit.v2.904.3.10.0-693.11.6.el7.x86_64/bin/shapeit
 
+for i in $CHR
+do
+	NAME=$(basename "$i")   #ESTO SE QUEDA SOLO CON NOMBRE DEL ARCHIVO, NO CON $PATH
+	NAME2=${NAME%.vcf.gz}_phased
+	NAME3=${NAME%.vcf.gz}
+	mkdir -p $OUT
+	$SHAPEIT --input-vcf $i \
+		-O $OUT/$NAME2 \
+		--window 0.5 \
+		-T 10 \
+		--force \
+		--input-map ${REC}$NAME3.txt
+	echo "Chromosome $NAME phased"
+	$SHAPEIT -convert \
+        	--input-haps $OUT/$NAME2 \
+        	--output-vcf $OUT/$NAME2.vcf &&
 
-parallel \
-shapeit --input-vcf /DATA/2-WorkData/juncos_reseq/2-FilteredBiallelicSNPs/236/{}.recode.vcf.gz \
-	-O /DATA/2-WorkData/juncos_reseq/2-FilteredBiallelicSNPs/236/phased/{}_phased \
-	--window 0.5 -T 20 --force "&&" \
-shapeit -convert --input-haps /DATA/2-WorkData/juncos_reseq/2-FilteredBiallelicSNPs/236/phased/{}_phased \
-	--output-vcf /DATA/2-WorkData/juncos_reseq/2-FilteredBiallelicSNPs/236/phased/{}_phased.vcf "&&" \
-bgzip /DATA/2-WorkData/juncos_reseq/2-FilteredBiallelicSNPs/236/phased/{}_phased.vcf "&&" \
-bcftools index /DATA/2-WorkData/juncos_reseq/2-FilteredBiallelicSNPs/236/phased/{}_phased.vcf.gz \
-:::: /mnt/6TB/1-Juncos/4-PopIndexes/2-Rehh/A_cmd/236/A_list/chr_list.txt
+	echo "Chromosome $NAME2 converted to .vcf"
 
+	bgzip $OUT/$NAME2.vcf &&
+	bcftools index $OUT/$NAME2.vcf.gz
+
+	echo "Chromosome $NAME2 bgzipped"
+done
