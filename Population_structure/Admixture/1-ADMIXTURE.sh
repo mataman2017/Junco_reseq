94cba444893e840c04ca32cfc06948e7b3c64d3c
Javier Sala-Garcia <33962787+mataman2017@users.noreply.github.com>
2024-07-03 11:45:15 +0200

    Update and rename 1-prepare_ADMIX.sh to 1-ADMIXTURE.sh

--- Population_structure/Admixture/1-ADMIXTURE.sh
+++ Population_structure/Admixture/1-ADMIXTURE.sh
@@ -8,3 +8,13 @@ plink2 --vcf $FILE.vcf.gz --make-bed --out $FILE --allow-extra-chr --const-fid
 # ADMIXTURE does not accept chromosome names that are not human chromosomes. We will thus just exchange the first column by 0
 awk '{$1=0;print $0}' $FILE.bim > $FILE.bim.tmp
 mv $FILE.bim.tmp $FILE.bim
+
+for K in 2 3 4 5 6 7 8 9 10 
+do
+
+    admixture --cv -B200 $FILE.bed $K | tee log${K}.out
+
+done
+
+grep -h CV log*.out > bestK.txt
+
